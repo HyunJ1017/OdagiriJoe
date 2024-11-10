925352168e112e5c34b9ead52bee93a6ac20a776
DESKTOP-DNRTKFK\slev5 <hyunj1017@naver.com>
2024-11-10 19:00:37 +0900

    1110 로그인 html파일

---
+++ src/main/resources/static/js/member-sign/logIn.js
@@ -0,0 +1,39 @@
+/* 로그인 */
+const logInBtn = document.querySelector("#logIn-btn");
+const inputPw = document.querySelector("#inputPw");
+const inputId = document.querySelector("#inputId");
+
+logInBtn.addEventListener("click", () => {
+
+  if(inputId.value.trim().length === 0){
+    return;
+  }
+
+  if(inputPw.value.trim().length === 0){
+    return;
+  }
+
+  goLogIn();
+});
+
+inputPw.addEventListener("keyup", (e)=> {
+  if(e.key === "Enter") goLogIn();
+})
+
+const goLogIn = () => {
+
+  const form = document.createElement("form");
+  form.action="/logIn";
+  form.method="POST";
+  const input1 = document.createElement("input");
+  input1.type = "hidden";
+  input1.value = inputId.value.trim();
+  input1.name = "memberId";
+  const input2 = document.createElement("input");
+  input2.type = "hidden";
+  input2.value = inputPw.value.trim();
+  input2.name = "memberPw";
+  form.append(input1,input2);
+  document.querySelector("body").append(form);
+  form.submit();
+}
