569b56964e777567cd0ed0ec630cc97b9ee4e951
HyunJ1017 <hyunj1017@naver.com>
2024-11-13 15:09:15 +0900

    작가등록 및 이미지업로드1/3

--- src/main/java/edu/kh/plklj/common/config/FireBaseConfig.java
+++ src/main/java/edu/kh/plklj/common/config/FireBaseConfig.java
@@ -2,9 +2,11 @@ package edu.kh.plklj.common.config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Bucket;
@@ -16,27 +18,31 @@ import com.google.firebase.cloud.StorageClient;
 @Configuration
 public class FireBaseConfig {
 
-//    @Bean
-//    public FirebaseApp firebaseApp() throws IOException {
-//        FileInputStream serviceAccount = 
-//            new FileInputStream("비공개 키 파일 경로");
-//
-//        FirebaseOptions options = FirebaseOptions.builder()
-//            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//            .setStorageBucket("storage 주소")
-//            .build();
-//
-//        FirebaseApp app = FirebaseApp.initializeApp(options);
-//        return app;
-//    }
-//
-//    @Bean
-//    public FirebaseAuth firebaseAuth() throws IOException {
-//        return FirebaseAuth.getInstance(firebaseApp());
-//    }
-//
-//    @Bean
-//    public Bucket bucket() throws IOException {
-//        return StorageClient.getInstance(firebaseApp()).bucket();
-//    }
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+    	 InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+
+    	// Check if FirebaseApp is already initialized
+         if (FirebaseApp.getApps().isEmpty()) {
+        	 FirebaseOptions options = FirebaseOptions.builder()
+        	            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        	            .setStorageBucket("odagirijoe-3e3a4.firebasestorage.app")
+        	            .build();
+
+        	        FirebaseApp app = FirebaseApp.initializeApp(options);
+        	        return app;
+         }
+         return FirebaseApp.getInstance();
+        
+    }
+
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+        return FirebaseAuth.getInstance(firebaseApp());
+    }
+
+    @Bean
+    public Bucket bucket() throws IOException {
+        return StorageClient.getInstance(firebaseApp()).bucket();
+    }
 }
