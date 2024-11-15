91570c09e80ac755a2f119cc259b6c3b6951d264
hojung7 <wjdghwjd99@naver.com>
2024-11-15 17:51:12 +0900

    20241115_관리자페이지 백엔드

--- src/main/java/edu/kh/plklj/manage/controller/ManageController.java
+++ src/main/java/edu/kh/plklj/manage/controller/ManageController.java
@@ -1,51 +1,56 @@
 package edu.kh.plklj.manage.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kh.plklj.manage.service.ManageService;
-import edu.kh.plklj.notification.controller.NotificationController;
-import edu.kh.plklj.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("manage")
 @Slf4j
 public class ManageController {
-	
+
 	private final ManageService service;
-	
-	// 관리자 페이지
+
 	@GetMapping("")
 	public String managePage() {
 		return "manage/manage";
+	}
+
+	// 콘텐츠 관리
+	@GetMapping("getList")
+	@ResponseBody
+	public Map<String, Object> getReportContents(@RequestParam("code") String code,
+		@RequestParam(name="cp", required = false, defaultValue = "1") int cp ){
 		
+		return service.getSearchList(code, cp);
 	}
 	
+
 	// 공지사항 수정 페이지
 	@GetMapping("revise")
 	public String reviseNotice() {
 		return "manage/noticeRevise";
 	}
-	
+
 	// 공지사항 작성 페이지
 	@GetMapping("write")
 	public String writeNotice() {
 		return "manage/noticeWrite";
 	}
-	
+
 	// 승인 요청 시 프로필 페이지
 	@GetMapping("confirm")
 	public String profileConfirm() {
 		return "manage/profileConfirm";
 	}
-	
-	
-	
 
 }
--- src/main/java/edu/kh/plklj/manage/service/ManageService.java
+++ src/main/java/edu/kh/plklj/manage/service/ManageService.java
@@ -1,5 +1,11 @@
 package edu.kh.plklj.manage.service;
 
+
+import java.util.Map;
+
 public interface ManageService {
+
+	
+	Map<String, Object> getSearchList(String code, int cp);
 
 }
