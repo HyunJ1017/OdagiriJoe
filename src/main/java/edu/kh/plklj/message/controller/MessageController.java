a5ea939adb0cf2c794c735c925053e314259bdc9
hojung7 <wjdghwjd99@naver.com>
2024-12-02 13:45:07 +0900

    20241202_승인 거절 시 문자 api(중간 커밋)

--- src/main/java/edu/kh/plklj/message/controller/MessageController.java
+++ src/main/java/edu/kh/plklj/message/controller/MessageController.java
@@ -1,11 +1,16 @@
 package edu.kh.plklj.message.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.kh.plklj.message.service.MessageService;
@@ -24,16 +29,17 @@ public class MessageController {
 		this.messageService = messageService;
 	}
 
-	/**
-	 * 문자 전송 API
-	 * 
-	 * @param phoneNumber 수신자의 전화번호
-	 * @param text        메시지 내용
-	 * @return 결과 메시지
-	 */
+	// 문자 전송 API
 	@PostMapping("/send")
-	public String sendMessage(@RequestParam String phoneNumber, @RequestParam String text) {
-		return messageService.sendSMS(phoneNumber, text);
+	@ResponseBody
+	public String sendRejectionMessage(@RequestBody int memberNo) {
+
+	    // 문자 전송
+	    int result = messageService.sendSMS(memberNo); // 문자 전송 서비스 호출
+	    if(result > 0) return "거절 및 문자 전송 성공";
+	    else return "실패";
 	}
+	
+	
 
 }
