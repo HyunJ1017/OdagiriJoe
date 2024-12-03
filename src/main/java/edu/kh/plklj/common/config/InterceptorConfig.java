12f5b8578903ac58676873133da37196f11dc0a8
sungyeopkim11 <777sy777@naver.com>
2024-12-03 15:49:52 +0900

    20241203 대쉬보드 마무리

--- src/main/java/edu/kh/plklj/common/config/InterceptorConfig.java
+++ src/main/java/edu/kh/plklj/common/config/InterceptorConfig.java
@@ -18,7 +18,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 		
 		// 특정 URL 패턴에만 적용
 		registry.addInterceptor(visitorInterceptor)
-						.addPathPatterns("/") // 메인 페이지
+						.addPathPatterns("/", "/main") // 메인 페이지
 						.excludePathPatterns("/static/**", "/css/**", "/js/**"); // 정적 리소스 제외
 	}
 }
